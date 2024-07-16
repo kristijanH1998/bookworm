c620b36be6c66ca90ee332f2f6f6b9bc5de0ac08
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-07-16 01:18:19 -0700

    Login page design edit

--- BookWorm/src/App.jsx
+++ BookWorm/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 
 function App() {
-
-  return (
-    
-      <form>
-        <div className="mb-3">
+  return (  
+      <form className="d-flex flex-column align-items-center">
+        <h1 className="mb-5">Login</h1>
+        <div className="mb-3 d-flex flex-column align-items-center">
           <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <div id="emailHelp" className="form-text">We&apos;ll never share your email with anyone else.</div>
         </div>
-        <div className="mb-3">
+        <div className="mb-3 d-flex flex-column align-items-center">
           <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
           <input type="password" className="form-control" id="exampleInputPassword1"/>
         </div>
@@ -18,9 +16,9 @@ function App() {
           <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
           <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary mb-4">Submit</button>
+        <button type="button" className="btn btn-success">Register</button>
       </form>
-    
   )
 }
 
