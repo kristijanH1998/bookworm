b60cc1df6057a84fbe262edb0bcb32102ada80e8
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-07-26 16:44:58 -0700

    created Sign Out button on Home page, connected to /log-out endpoint on backend that removes jwt key from local storage when activated

--- BookWorm/src/Home.jsx
+++ BookWorm/src/Home.jsx
@@ -1,5 +1,29 @@
-export default function Login() {
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
+
+export default function Home() {
+    const navigate = useNavigate();
+    const handleClick = (event) => {
+        event.preventDefault();
+        // console.log("You clicked" + event.target)
+        let jwt = localStorage.getItem("jwt");
+        // console.log(jwt)
+        axios
+            .get("http://localhost:3000/log-out", {headers: {"authorization": "Bearer " + jwt}})
+            .then((res) => {
+                if (res.data.success) {
+                    localStorage.removeItem("jwt");
+                    navigate("/");
+                } 
+            })
+            .catch((error) => {
+                console.log(error.response.data.error)
+            });
+    }
     return (
+        <>
         <h1>Home Page</h1>
+        <Link type="button" onClick={handleClick} className="btn">Sign Out</Link>
+        </>    
     )
 }
