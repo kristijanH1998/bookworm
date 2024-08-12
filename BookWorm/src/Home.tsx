e17289a7ea279dd55924396fc998cca032bd7442
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-08-12 03:25:54 -0700

    form for searching books made; reactive radio buttons connected to search bar placeholder

--- BookWorm/src/Home.tsx
+++ BookWorm/src/Home.tsx
@@ -1,8 +1,14 @@
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import React from 'react';
+import { useState } from "react";
 
 export default function Home() {
+    const [searchPlaceholder, setSearchPlaceholder] = useState("title");
+    const onRadioChange = (e: any) => {
+        setSearchPlaceholder(e.target.value)
+    }
+
     const navigate = useNavigate();
     const handleClick = (event: any) => {
         event.preventDefault();
@@ -28,9 +34,32 @@ export default function Home() {
             });
     }
     return (
-        <>
-        <h1>Home Page</h1>
-        <Link type="button" onClick={handleClick} className="btn" to={''}>Sign Out</Link>
-        </>    
+        <form id="searchPage" className="d-flex flex-column align-items-center w-50">
+            <h1>Search Books</h1>
+            <div className="d-flex flex-row m-3">
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="title" 
+                    checked={searchPlaceholder === "title"} onChange={onRadioChange}/>
+                    <label className="form-check-label" htmlFor="inlineRadio1">Title</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="author"
+                    checked={searchPlaceholder === "author"} onChange={onRadioChange}/>
+                    <label className="form-check-label" htmlFor="inlineRadio2">Author</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="ISBN"
+                    checked={searchPlaceholder === "ISBN"} onChange={onRadioChange}/>
+                    <label className="form-check-label" htmlFor="inlineRadio3">ISBN</label>
+                </div>
+            </div>
+            
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder={"Enter book " + searchPlaceholder} aria-label="Enter book parameters" aria-describedby="button-addon2"/>
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+            </div>
+
+            {/* <Link type="button" onClick={handleClick} className="btn" to={''}>Sign Out</Link> */}
+        </form>    
     )
 }
