2af141950f645fb28da7030ffe7cdd202fb610a8
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-10-10 19:41:57 -0700

    My Profile page now shows all relevant user's data; page connected to /user-data backend endpoint; fetches data on page load

--- BookWorm/src/MyProfile.tsx
+++ BookWorm/src/MyProfile.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 export default function MyProfile() {
     
     const [jwt, setJwt] = useState<string | null>("");
+    const [userData, setUserData] = useState<object>({});
 
     function acquireJwt () {
         if(localStorage.getItem("jwt")) {
@@ -53,72 +54,91 @@ export default function MyProfile() {
         }
     };
     
-    
+    useEffect(() => {
+        acquireJwt();
+    },[])
+
+    useEffect(() => {
+        // console.log(jwt)
+        if(jwt) { 
+            axios
+                .get("http://localhost:3000/user-data", {headers: {"authorization": "Bearer " + jwt}})
+                .then((res) => {
+                    if (res.data.success) {
+                        // console.log(res.data.data[0]);
+                        setUserData(res.data.data[0]);
+                    } 
+                })
+                .catch((error) => {
+                    console.log(error.response.data.error)
+                });
+        }
+    }, [jwt])
     
     return (
         <div className="d-flex flex-column align-items-center py-4 w-75">
             <div className="d-flex flex-column align-items-center py-4 w-75">
-            <nav className="navbar d-flex w-100">
-                <form className="container-fluid justify-content-center">
-                    {/* <button className="btn btn-outline-success me-3" type="button">My Books</button> */}
-                    <Link type="button" onClick={goToPage} className="btn me-3" to={'/home'} id="searchBooksBtn">Search Books</Link>
-                    <Link type="button" onClick={goToPage} className="btn me-3" to={'/my-books'} id="myBooksBtn">My Books</Link>
-                    <Link type="button" onClick={goToPage} className="btn" to={''} id="signOutBtn">Sign Out</Link>
-                </form>
-            </nav>
-            <form className="d-flex flex-column align-items-center w-100 p-3">
-                <h1 className="my-3">My Profile</h1>
-                <div className="container w-100">
-                    <div className="row">
-                        <div className="mb-3">
-                            <label  className="form-label">Email Address</label>
-                            <div className="input-group">
-                                <span className="input-group-text w-100">Email</span>
+                <nav className="navbar d-flex w-100">
+                    <form className="container-fluid justify-content-center">
+                        {/* <button className="btn btn-outline-success me-3" type="button">My Books</button> */}
+                        <Link type="button" onClick={goToPage} className="btn me-3" to={'/home'} id="searchBooksBtn">Search Books</Link>
+                        <Link type="button" onClick={goToPage} className="btn me-3" to={'/my-books'} id="myBooksBtn">My Books</Link>
+                        <Link type="button" onClick={goToPage} className="btn" to={''} id="signOutBtn">Sign Out</Link>
+                    </form>
+                </nav>
+                <form className="d-flex flex-column align-items-center w-100 p-3">
+                    <h1 className="my-3">My Profile</h1>
+                    <div className="container w-100">
+                        <div className="row">
+                            <div className="mb-3">
+                                <label  className="form-label">Email Address:</label>
+                                <div className="input-group">
+                                    <span className="input-group-text w-100">{userData["email"] ? userData["email"] : "N/A"}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mb-3">
-                            <label  className="form-label">Username</label>
-                            <div className="input-group">
-                                <span className="input-group-text w-100">Username</span>
+                            <div className="mb-3">
+                                <label  className="form-label">Username:</label>
+                                <div className="input-group">
+                                    <span className="input-group-text w-100">{userData["user_name"] ? userData["user_name"] : "N/A"}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mb-3">
-                            <label  className="form-label">First Name</label>
-                            <div className="input-group">
-                                <span className="input-group-text w-50">First Name</span>
-                                <input type="text" className="form-control" placeholder="Type here to change your First Name"/>
-                                <button className="btn btn-outline-secondary" type="button">Update</button>
+                            <div className="mb-3">
+                                <label  className="form-label">First Name:</label>
+                                <div className="input-group">
+                                    <span className="input-group-text w-50">{userData["first_name"] ? userData["first_name"] : "N/A"}</span>
+                                    <input type="text" className="form-control" placeholder="Type here to change your First Name"/>
+                                    <button className="btn btn-outline-secondary" type="button">Update</button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mb-3">
-                            <label  className="form-label">Last Name</label>
-                            <div className="input-group">
-                                <span className="input-group-text w-50" >Last Name</span>
-                                <input type="text" className="form-control" placeholder="Type here to change your Last Name"/>
-                                <button className="btn btn-outline-secondary" type="button">Update</button>
+                            <div className="mb-3">
+                                <label  className="form-label">Last Name:</label>
+                                <div className="input-group">
+                                    <span className="input-group-text w-50" >{userData["last_name"] ? userData["last_name"] : "N/A"}</span>
+                                    <input type="text" className="form-control" placeholder="Type here to change your Last Name"/>
+                                    <button className="btn btn-outline-secondary" type="button">Update</button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Date of Birth</label>
-                            <div className="input-group">
-                                <span className="input-group-text w-50">Date of Birth</span>
-                                <input type="text" onFocus={(event) => {event.target.type="date"}} onBlur={(event) => {event.target.type="text"}} className="form-control" placeholder="Click here to change your DoB"/>
-                                <button className="btn btn-outline-secondary" type="button">Update</button>
+                            <div className="mb-3">
+                                <label className="form-label">Date of Birth:</label>
+                                <div className="input-group">
+                                    <span className="input-group-text w-50">{userData["date_of_birth"] ? userData["date_of_birth"].substring(0, 10) : "N/A"}</span>
+                                    <input type="text" onFocus={(event) => {event.target.type="date"}} onBlur={(event) => {event.target.type="text"}} className="form-control" placeholder="Click here to change your DoB"/>
+                                    <button className="btn btn-outline-secondary" type="button">Update</button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Password</label>
-                            <div className="input-group">
-                                <input type="password" name="password" className="form-control" placeholder="To change Password, enter your Current Password"/>
-                                <input type="password" name="password" className="form-control" placeholder="Now enter your New Password"/>
-                                <button className="btn btn-outline-secondary" type="button">Submit</button>
+                            <div className="mb-3">
+                                <label className="form-label">Password:</label>
+                                <div className="input-group">
+                                    <input type="password" name="password" className="form-control" placeholder="To change Password, enter your Current Password"/>
+                                    <input type="password" name="password" className="form-control" placeholder="Now enter your New Password"/>
+                                    <button className="btn btn-outline-secondary" type="button">Submit</button>
+                                </div>
                             </div>
+    
                         </div>
-   
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
         </div>
     )
 }
